f03f8b2c642df02ea5cb749dc497ca3e3104521f
ljd <ljubicadordevic00@gmail.com>
2025-11-06 16:40:00 +0100

    initial ESPKconfig + Bsp. Spezifikation aus der Arbeit

--- exp/test.py
+++ exp/test.py
@@ -1,5 +1,7 @@
-import kconfiglib
+#import kconfiglib
+from  esp_kconfiglib import Kconfig as kconfiglib
 
-kconf = kconfiglib.Kconfig('Kconfig')
+#kconf = kconfiglib.Kconfig('Kconfig')
+kconf = kconfiglib('KconfigEsp')
 print('Symbols:', len(kconf.defined_syms))
 
