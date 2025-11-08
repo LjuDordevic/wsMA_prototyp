f60bca39764ff17df87593609139896374837dfe
ljd <ljubicadordevic00@gmail.com>
2025-11-08 14:11:58 +0100

    parser - remove syntax errors & add test + print

--- core/pick_parser.py
+++ core/pick_parser.py
@@ -1,6 +1,7 @@
 import sys 
 import os
 from pathlib import Path
+import pprint
 
 # /core  
 base_dir = Path(__file__).parent
@@ -134,6 +135,12 @@ class PickParser:
             kconf = self.kconfiglib.Kconfig(str(kconfig_file))
         print('Symbols: ', len(kconf.defined_syms))
 
+    def _parse_only(self, project_dir: str, kconfig_file: str):
+        if self.spec_version != "ZRTOS":
+            raise ValueError("FOR ZRTOS")
+        parser = ZRTOSParser(self.kconfiglib)
+        return parser.parse_files(project_dir, kconfig_file)
+
 class ZRTOSParser:
     """
     help-class -> Parser for Zephyr RTOS specifications 
@@ -171,48 +178,65 @@ class ZRTOSParser:
             
             def _finalize_sym(self, sym):
                 if hasattr(self, '_parse_only') and self._parse_only:
-                    print("skip _finalize_sym: taakes care of configdefault")
+                    print("skip _finalize_sym: takes care of configdefault")
                     return
-                return super()._finalize_sym(self, sym)
+                return super()._finalize_sym(sym)
             
             # some of these depend on everything being finalized 
             # TODO: check workaround 
-            def _check_sym_sanity():
+            def _check_sym_sanity(self):
                 if hasattr(self, '_parse_only') and self._parse_only:
                     print("skip _check_sym_synity")
                     return
                 return super()._check_sym_sanity()
             
-            def _check_choice_sanity():
+            def _check_choice_sanity(self):
                 if hasattr(self, '_parse_only') and self._parse_only:
                     print("skip _check_choice_sanity")
                     return
                 return super()._check_choice_sanity()
             
-            def _check_undef_syms():
+            def _check_undef_syms(self):
                 if hasattr(self, '_parse_only') and self._parse_only:
                     print("skip _check_undef_syms")
                     return
                 return super()._check_undef_syms()
             
-            def _check_undef_syms():
+            def _check_undef_syms(self):
                 if hasattr(self, '_parse_only') and self._parse_only:
                     print("skip _check_undef_syms")
                     return
                 return super()._check_undef_syms()
 
-            def _build_dep():
+            def _build_dep(self):
                 if hasattr(self, '_parse_only') and self._parse_only:
                     print("skip _build_dep")
                     return
                 return super()._build_dep()
             
-            def _add_choice_deps():
+            def _add_choice_deps(self):
                 if hasattr(self, '_parse_only') and self._parse_only:
                     print("skip _add_choice_deps")
                     return
                 return super()._add_choice_deps()    
 
+        kconf = KconfigParser(kconfig_file)
+
+        return {
+            'kconf': kconf,
+            'top_node': kconf.top_node,
+            'syms': kconf.syms,
+            'const_sysm': kconf.const_syms,
+            'defined_sysm': kconf.defined_syms,
+            'missing_syms': kconf.missing_syms,
+            'named_choices': kconf.named_choices,
+            'choices': kconf.choices,
+            'menus': kconf.menus,
+            'comments': kconf.comments,
+            'unique_defined_syms': kconf.unique_defined_syms,
+            'unique_choices': kconf.unique_choices
+        }
+    
 
 if __name__ == "__main__":
   
@@ -220,7 +244,9 @@ if __name__ == "__main__":
     print(picker_zrtos)
     picker_zrtos._test_kconfiglib("/home/ljd/wsMA_prototyp/exp", "KconfigZephyrRTOS")
     print("-" * 50)
-    
+    result = picker_zrtos._parse_only("/home/ljd/wsMA_prototyp/exp", "KconfigZephyrRTOS")
+    pprint.pprint(result)
+    """ 
     picker_zkcl = PickParser("ZKCL")
     print(picker_zkcl)
     picker_zkcl._test_kconfiglib("/home/ljd/wsMA_prototyp/exp", "Kconfig")
@@ -229,3 +255,4 @@ if __name__ == "__main__":
     picker_esp = PickParser("ESPIDF")
     print(picker_esp)
     picker_esp._test_kconfiglib("/home/ljd/wsMA_prototyp/exp", "KconfigEsp")
+    """
