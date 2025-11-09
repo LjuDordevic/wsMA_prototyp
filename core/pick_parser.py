848dec94e49042e901b2cdbb1f00af2269eeef3c
ljd <ljubicadordevic00@gmail.com>
2025-11-09 17:23:38 +0100

    clean up - comments, update attribute names

--- core/pick_parser.py
+++ core/pick_parser.py
@@ -14,28 +14,28 @@ class PickParser:
     __slots__= (
         "spec_version",
         "_kconfig_folder",
-        "kconfiglib"
+        "kconfiglib_version"
     )
 
     def __str__(self):
         return (
-            f"spec_version: {self.spec_version}\n"
-            f"kconfig_folder: {self._kconfig_folder}\n"
-            f"kconfiglib: {self.kconfiglib}"
+            f"START using parser: \n"
+            f"  spec_version: {self.spec_version}\n"
+            f"  kconfig_folder: {self._kconfig_folder}\n"
+            f"  kconfiglib_version: {self.kconfiglib_version}"
         )
 
     def __init__(self, spec_version: str):
         """
-        Init ParserPicker based on given specification version (specification = main Kconfig file)
+        Init ParserPicker based on given specification version (specification = Kconfig file)
         valid specification versions are ("ZKCL", "ZRTOS", "ESPIDF")
         ZKCL    = Kconfiglib specification                  for zephyr-rtos/Kconfiglib
         ZRTOS   = project specific Kconfiglib specification for zephyr-rtos/zephyr 
         ESPIDF  = project specific Kconfiglib specification for espressif/esp-idf-kconfig
         """
-
         self.spec_version = spec_version.upper() # make sure is upper case 
         self._kconfig_folder = None
-        self.kconfiglib = None
+        self.kconfiglib_version = None
         self._load_kconfiglib()
     
     def _load_zrtos(self):
@@ -56,7 +56,7 @@ class PickParser:
 
         try:
             import kconfiglib as kconfiglib_zrtos
-            self.kconfiglib = kconfiglib_zrtos
+            self.kconfiglib_version = kconfiglib_zrtos
         except ImportError as e:
             raise ImportError(f"couldn't import from {kcl_folder_str}: {e}")
 
@@ -83,7 +83,7 @@ class PickParser:
 
         try:
             import kconfiglib as z_kconfiglib
-            self.kconfiglib = z_kconfiglib
+            self.kconfiglib_version = z_kconfiglib
         except ImportError as e:
             raise ImportError(f"couldn't import from {kcl_folder_str}: {e}")    
 
@@ -101,7 +101,7 @@ class PickParser:
 
         try:
             from esp_kconfiglib import Kconfig as esp_kconfiglib
-            self.kconfiglib = esp_kconfiglib
+            self.kconfiglib_version = esp_kconfiglib
         except ImportError as e:
             raise ImportError(f"couldn't import from {kcl_folder_str}: {e}")
 
@@ -132,26 +132,26 @@ class PickParser:
 
         if self.spec_version == "ESPIDF":
             print("esp file path: " + str(esp_kconfig_file))
-            kconf = self.kconfiglib(str(esp_kconfig_file))
+            kconf = self.kconfiglib_version(str(esp_kconfig_file))
         else:
-            kconf = self.kconfiglib.Kconfig(str(kconfig_file))
+            kconf = self.kconfiglib_version.Kconfig(str(kconfig_file))
         print('Symbols: ', len(kconf.defined_syms))
 
     def _parse_only(self, project_dir: str, kconfig_file: str):
         if self.spec_version != "ZRTOS":
             raise ValueError("FOR ZRTOS")
-        parser = ZRTOSParser(self.kconfiglib)
+        parser = ZRTOSParser(self.kconfiglib_version)
         return parser.parse_files(project_dir, kconfig_file)
-
+#----------------------------------------------------------
 if __name__ == "__main__":
   
     picker_zrtos = PickParser("ZRTOS")
     print(picker_zrtos)
     picker_zrtos._test_kconfiglib("/home/ljd/wsMA_prototyp/exp", "KconfigZephyrRTOS")
     print("-" * 50)
-    result = picker_zrtos._parse_only("/home/ljd/wsMA_prototyp/exp", "KconfigZephyrRTOS")
-    pprint.pprint(result)
-    """ 
+    #result = picker_zrtos._parse_only("/home/ljd/wsMA_prototyp/exp", "KconfigZephyrRTOS")
+    #pprint.pprint(result)
+
     picker_zkcl = PickParser("ZKCL")
     print(picker_zkcl)
     picker_zkcl._test_kconfiglib("/home/ljd/wsMA_prototyp/exp", "Kconfig")
@@ -160,4 +160,3 @@ if __name__ == "__main__":
     picker_esp = PickParser("ESPIDF")
     print(picker_esp)
     picker_esp._test_kconfiglib("/home/ljd/wsMA_prototyp/exp", "KconfigEsp")
-    """
