02a35f87a94e843499a4a629c6fbcce86b716fb1
ljd <ljubicadordevic00@gmail.com>
2025-11-22 19:50:10 +0100

    zrtos parser - clean up

--- core/zrtos_parser.py
+++ core/zrtos_parser.py
@@ -5,7 +5,8 @@ class ZRTOSParser:
     """
     help-class -> Parser for Zephyr RTOS specifications 
     init: save kconfiglib_module that the PickParser chose -> input for subclass 
-    in the subclass: call the Kconfig from the kconfiglib_module + override functions 
+    in the subclass: call the parser from parent Kconfig 
+    we don't override finalize functions, because we want final result
     """
     def __init__(self, kconfiglib_module):
         self.kconfiglib = kconfiglib_module
@@ -13,76 +14,21 @@ class ZRTOSParser:
     def parse_files(self, project_dir: str, kconfig_file: str):
         project_dir_path = Path(project_dir)
         assert(project_dir_path).exists(), f"{project_dir_path} not found"
-        os.environ["srctree"] = str(project_dir_path)
-        os.environ["exp"] = "exp"
+        # don't need these now when we run skrip as target 
+            #os.environ["srctree"] = str(project_dir_path)  
+            #os.environ["exp"] = "exp"
 
         kconfig_file_path = project_dir_path / kconfig_file
         assert(kconfig_file_path).exists(), f"{kconfig_file} not found in {project_dir_path}"
         
         Kconfig = self.kconfiglib.Kconfig   # parent Kconfig class from (ZRTOS) kconfiglib_module
 
+        # this class was initially used to overwritte some classes in parent Kconfig class, 
+        # but this is not needed anymore, because we do want that the parser has parsed and finalized everything 
+        # so that we build a dictionary from the result and can later filter it 
         class KconfigParser(Kconfig):
             def __init__(self, filename):
-                self._parse_only = True     # flag used for override 
                 super().__init__(filename)  # call init from parent Kconfig
-
-            """ 
-            def _finalize_node(self, node, visible):
-                
-                only when class has attribute _parse_only and it's = True -> set _parsing_complete = True
-                else: call _finalize_node() like in parent Kconfig class
-                
-                if hasattr(self, '_parse_only') and self._parse_only:
-                    print("skip _finalize_node")
-                    self._parsing_complete = True
-                    return
-                return super()._finalize_node(node, visible)
-            
-            def _finalize_sym(self, sym):
-                if hasattr(self, '_parse_only') and self._parse_only:
-                    print("skip _finalize_sym: takes care of configdefault")
-                    return
-                return super()._finalize_sym(sym)
-            """
-            # some of these depend on everything being finalized 
-            # TODO: check workaround 
-            """ 
-            def _check_sym_sanity(self):
-                if hasattr(self, '_parse_only') and self._parse_only:
-                    print("skip _check_sym_synity")
-                    return
-                return super()._check_sym_sanity()
-            
-            def _check_choice_sanity(self):
-                if hasattr(self, '_parse_only') and self._parse_only:
-                    print("skip _check_choice_sanity")
-                    return
-                return super()._check_choice_sanity()
-            
-            def _check_undef_syms(self):
-                if hasattr(self, '_parse_only') and self._parse_only:
-                    print("skip _check_undef_syms")
-                    return
-                return super()._check_undef_syms()
-            
-            def _check_undef_syms(self):
-                if hasattr(self, '_parse_only') and self._parse_only:
-                    print("skip _check_undef_syms")
-                    return
-                return super()._check_undef_syms()
-
-            def _build_dep(self):
-                if hasattr(self, '_parse_only') and self._parse_only:
-                    print("skip _build_dep")
-                    return
-                return super()._build_dep()
-            
-            def _add_choice_deps(self):
-                if hasattr(self, '_parse_only') and self._parse_only:
-                    print("skip _add_choice_deps")
-                    return
-                return super()._add_choice_deps()   
-                """ 
 
         kconf = KconfigParser(kconfig_file)
         # dictionary key: value 
