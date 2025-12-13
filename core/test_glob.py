1cd33c55bb32472f1e55de9a92df98a33aaa5bba
ljd <ljubicadordevic00@gmail.com>
2025-12-13 19:36:30 +0100

    test glob

--- core/test_glob.py
+++ core/test_glob.py
@@ -3,13 +3,15 @@ from pathlib import Path
 
 test_dir = Path("/home/ljd/testglob")
 
-stern_pattern = str(test_dir / "*")
+stern_pattern = str(test_dir / "*/**")
 negation_pattern = str(test_dir / "[!cde].*")
 frage_pattern = str(test_dir / "e?*")
 
-#result = sorted(iglob(stern_pattern))
-result = sorted(iglob(negation_pattern))
+result = sorted(iglob(stern_pattern))
+#result = sorted(iglob(stern_pattern, recursive=True))
+#result = sorted(iglob(negation_pattern))
 #result = sorted(iglob(frage_pattern))
+#result = sorted(iglob("~/testglob/*"))
 
 print(len(result))
 for element in result:
