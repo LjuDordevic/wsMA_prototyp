2982e0cf5ea0f3d5f9bb5401c1a5db602cf5e245
ljd <ljubicadordevic00@gmail.com>
2025-12-28 23:12:35 +0100

    transform_def

--- core/workflow_test.py
+++ core/workflow_test.py
@@ -25,13 +25,13 @@ import sys
 def main():
 
     #log_file="/home/ljd/wsMA_prototyp/test_dir_/transform_source_output/transform_log_resolve_steps.log"
-    log_file = "/home/ljd/wsMA_prototyp/test_dir_/transform_source_output/transform.log"
-    project_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_source"       
-    output_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_source_output"  
+    #log_file = "/home/ljd/wsMA_prototyp/test_dir_/transform_source_output/transform.log"
+    #project_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_source"       
+    #output_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_source_output"  
 
-    #log_file = "/home/ljd/wsMA_prototyp/test_dir_/transform_def_output/transform.log"
-    #project_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_def"       
-    #output_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_def_output"  
+    log_file = "/home/ljd/wsMA_prototyp/test_dir_/transform_def_output/transform.log"
+    project_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_def"       
+    output_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_def_output"  
 
     #log_file="/home/ljd/wsMA_prototyp/test_dir_/transform_option_output/transform.log"
     #project_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_option"       
