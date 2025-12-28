4eb0a666d32e11abefa5226893151a1f50119ee6
ljd <ljubicadordevic00@gmail.com>
2025-12-28 23:07:26 +0100

    transform_source - detailed output

--- core/workflow_test.py
+++ core/workflow_test.py
@@ -24,14 +24,10 @@ import sys
 
 def main():
 
-    #log_file = "/home/ljd/wsMA_prototyp/test_dir_/test_glob/test_glob.log"
-    #log_file="/home/ljd/wsMA_prototyp/test_dir_/transform_source_output/transform_log_resolve_steps.log"
-    #project_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_source"       
-    #output_dir = "/home/ljd/wsMA_prototyp/test_dir_/test_glob"  
-
+    log_file="/home/ljd/wsMA_prototyp/test_dir_/transform_source_output/transform_log_resolve_steps.log"
     #log_file = "/home/ljd/wsMA_prototyp/test_dir_/transform_source_output/transform.log"
-    #project_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_source"       
-    #output_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_source_output"  
+    project_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_source"       
+    output_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_source_output"  
 
     #log_file = "/home/ljd/wsMA_prototyp/test_dir_/transform_def_output/transform.log"
     #project_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_def"       
@@ -46,9 +42,9 @@ def main():
     #project_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_choice"
     #output_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_choice_output"
 
-    log_file="/home/ljd/wsMA_prototyp/test_dir_/transform_configdefault_output/transform.log"
-    project_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_configdefault"
-    output_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_configdefault_output"
+    #log_file="/home/ljd/wsMA_prototyp/test_dir_/transform_configdefault_output/transform.log"
+    #project_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_configdefault"
+    #output_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_configdefault_output"
     
     main_file = "Kconfig"  
     os.environ["srctree"] = project_dir
@@ -104,7 +100,7 @@ def main():
         output_dir=Path(output_dir),
         log=True,
         log_lines=False,
-        log_and_check_resolve_glob=False,
+        log_and_check_resolve_glob=True,
         log_excel_after_each_file=False,
         log_excel_output="/home/ljd/wsMA_prototyp/results.xlsx"
     )    
