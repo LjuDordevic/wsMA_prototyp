e509e667dc66770a27140a2ca86b7246783c3047
ljd <ljubicadordevic00@gmail.com>
2025-12-30 21:40:15 +0100

    excel writer update

--- core/excel_writer.py
+++ core/excel_writer.py
@@ -4,7 +4,13 @@ def write_to_excel(data, output_path):
     wb = openpyxl.Workbook()
     ws = wb.active
 
-    headers = ['test file', 'input', 'output', 'source_keyword', 'osource_keyword', 'rource_keyword', 'orource_keyword', 'new lines bc source', 'new lines bc cd']
+    headers = ['test file', 'input', 'output', \
+               'source_keyword_wo_glob', 'source_keyword_w_glob',\
+                'osource_keyword', 'rource_keyword', 'orource_keyword', \
+                    'sum_all_source', 'option_env', 'new_lines_bc_of_def_', \
+                        'new_lines_bc_of_glob', 'new_lines_bc_cd', 'new_lines_bc_named_choice'\
+                            'removed_bc_orsource', 'removed_lines_bc_cd', 'removed_lines_bc_named_choice'\
+                                'removed_optional_choice_attr', 'removed_bool_choice_attr', 'removed_tristate_choice_attr']
     ws.append(headers)
 
     for row_data in data:
