ca4fd51982f83853c43c23b92238caf2af1ad17e
ljd <ljubicadordevic00@gmail.com>
2026-01-02 10:40:31 +0100

    timer

---
+++ core/time_writer.py
@@ -0,0 +1,21 @@
+import time 
+
+class TransformTimer:
+    def __init__(self):
+        self.start_time = time.time()
+        self.laps = []        
+
+    def lap(self, label):
+        now = time.time()
+        elapsed = now - self.start_time
+        self.laps.append((label, elapsed))
+        return elapsed
+    
+    def stop(self):
+        total = time.time() - self.start_time
+
+        print("\n" + "=" * 100)
+        print("TIMER REPORT:")
+        print("\n" + "=" * 100)
+
+        
