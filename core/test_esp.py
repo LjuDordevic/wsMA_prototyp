81af9d824209994f93344657ff145228da8175d9
ljd <ljubicadordevic00@gmail.com>
2026-01-04 20:04:04 +0100

    try testing espidf

--- core/test_esp.py
+++ core/test_esp.py
@@ -1,12 +1,17 @@
 from pathlib import Path
 from pick_parser import PickParser
 from espidf_parser import ESPIDFParser
+from kconfig_writer import KconfigReader, KconfigWriter
+from transform_prototyp import KconfigTransformer
+from time_writer import TransformTimer
 import sys
 import os 
 
 def main():    
-    log_file = "/home/ljd/wsMA_prototyp/testESPIDF/transform.log"
+    log_file = "/home/ljd/wsMA_prototyp/transform_projects/transform_esp_idf/transform.log"
+    excel_file = "/home/ljd/wsMA_prototyp/transform_projects/transform_esp_idf/results.xlsx"
     project_dir = "/home/ljd/espcode/v5.5.2/esp-idf"
+    output_dir = "/home/ljd/wsMA_prototyp/transform_projects/transform_esp_idf/ESP_IDF_demo_output"
     main_file = "Kconfig"
 
     os.environ["IDF_ENV_FPGA"] = "false"
@@ -15,6 +20,7 @@ def main():
     os.environ["COMPONENT_KCONFIGS_SOURCE_FILE"] = "/home/ljd/wsMA_prototyp/ESP_IDF_demo/build/kconfigs.in"
     os.environ["COMPONENT_KCONFIGS_PROJBUILD_SOURCE_FILE"] = "/home/ljd/wsMA_prototyp/ESP_IDF_demo/build/kconfigs_projbuild.in"
 
+    timer = TransformTimer()
     sys.stdout = open(log_file, "w")
     sys.stderr = sys.stdout 
 
@@ -23,7 +29,7 @@ def main():
     print("=" * 100)
     print(f"Root: {project_dir}")
     print(f"Main file: {main_file}")
-    #print(f"Output dir: {output_dir}")
+    print(f"Output dir: {output_dir}")
     print("=" * 100)
     
     picker = PickParser("ESPIDF")
@@ -40,6 +46,39 @@ def main():
         import traceback
         traceback.print_exc()
         sys.exit(1)
+
+    transformer = KconfigTransformer(source_spec="ZRTOS")
+    print("\n2. Bild ExtParserContext FROM PARSER RESULTS")
+    print(f" Transformer used: {transformer.source_spec}")
+
+    context = transformer._build_context_from_parser(
+        parser_result,
+        log=False
+    )
+    
+    timer.lap("Built context from parser results")
+    print("=" * 100)
+    print(f"Build context finished")
+
+    reader = KconfigReader("ESPIDF")
+    writer = KconfigWriter("ESPIDF")
+
+    excel_data = transformer.transform_all_files(
+        reader=reader,
+        writer=writer,
+        project_dir=Path(project_dir),
+        output_dir=Path(output_dir),
+        log=True,
+        log_lines=True,
+        log_and_check_resolve_glob=True,
+        log_cd_nc_details=False,
+        log_excel_after_each_file=True,
+        log_excel_output=excel_file
+    )   
+
+    timer.lap("File transformation and excel log")
+
+    timer.stop()
 
     return 0
 
