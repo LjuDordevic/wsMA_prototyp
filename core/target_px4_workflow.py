9feb426e92fb9982649d018ac7e257caa92628ed
ljd <ljubicadordevic00@gmail.com>
2026-01-06 12:15:34 +0100

    PX4-Autopilot Transformation  2.

--- core/target_px4_workflow.py
+++ core/target_px4_workflow.py
@@ -10,8 +10,8 @@ import pprint
 import os
 
 def main():    
-    log_file = "/home/ljd/wsMA_prototyp/transform_projects/transform_px4/PX4_log/transform.log"
-    excel_file = "/home/ljd/wsMA_prototyp/transform_projects/transform_px4/PX4_log/results.xlsx"
+    log_file = "/home/ljd/wsMA_prototyp/transform_projects/transform_px4/PX4_log/transform2.log"
+    excel_file = "/home/ljd/wsMA_prototyp/transform_projects/transform_px4/PX4_log/results2.xlsx"
     project_dir = "/home/ljd/px4/PX4-Autopilot"
     output_dir = "/home/ljd/wsMA_prototyp/transform_projects/transform_px4/PX4_output"
     main_file = "Kconfig"
