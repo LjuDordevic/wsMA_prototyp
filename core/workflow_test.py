e6b736d5c940dca6371ba641bb126b60ccfb6d14
ljd <ljubicadordevic00@gmail.com>
2026-01-09 16:28:55 +0100

    bsp

--- core/workflow_test.py
+++ core/workflow_test.py
@@ -41,17 +41,17 @@ def main():
     #output_dir = "/home/ljd/wsMA_prototyp/test_dir_/transform_option_output"  
     #os.environ["ENV_A"] = "i7-1260P"
 
-    log_file="/home/ljd/wsMA_prototyp/test_dir_/transform_choice_output/transform.log"
-    project_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_choice"
-    output_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_choice_output"
+    #log_file="/home/ljd/wsMA_prototyp/test_dir_/transform_choice_output/transform.log"
+    #project_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_choice"
+    #output_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_choice_output"
 
     #log_file="/home/ljd/wsMA_prototyp/test_dir_/transform_configdefault_output/transform.log"
     #project_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_configdefault"
     #output_dir="/home/ljd/wsMA_prototyp/test_dir_/transform_configdefault_output"
     
-    #log_file = "/home/ljd/wsMA_prototyp/transform_projects/transform_zrtos/ZRTOS_log/output_analysis/exp_zep.log"
-    #project_dir = "/home/ljd/wsMA_prototyp/transform_projects/transform_zrtos/ZRTOS_log/output_analysis/exp_zep"
-    #output_dir = "/home/ljd/wsMA_prototyp/transform_projects/transform_zrtos/ZRTOS_log/output_analysis/exp_zep_output"
+    log_file = "/home/ljd/wsMA_prototyp/transform_projects/transform_zrtos/ZRTOS_log/output_analysis/exp_zep.log"
+    project_dir = "/home/ljd/wsMA_prototyp/transform_projects/transform_zrtos/ZRTOS_log/output_analysis/exp_zep"
+    output_dir = "/home/ljd/wsMA_prototyp/transform_projects/transform_zrtos/ZRTOS_log/output_analysis/exp_zep_output"
 
     main_file = "Kconfig"  
     os.environ["srctree"] = project_dir
